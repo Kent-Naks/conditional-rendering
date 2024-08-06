ac962177b7597aaa5693a1f5d92b7282662ae40e
Kent Eugine Kariuki <eugenekariuki@gmail.com>
2024-08-06 15:00:12 +0300

    Adds

--- conditional-render/src/index.js
+++ conditional-render/src/index.js
@@ -1,17 +1,9 @@
+// Import React and ReactDOM libraries
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
+// Import the main App component
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Render the App component inside the root div in index.html
+ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
